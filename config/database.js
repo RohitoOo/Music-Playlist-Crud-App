663cd42f702e961d2407c14bdc585f9f1961e506
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-08-04 19:07:21 +0530

    Heroku Updated Mongodb

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-database:'mongodb://localhost:27017/songsdatabase' ,
+database:'mongodb://heroku_gq6tvx1v:d1ulefa3nhhup7akbalbaegoup@ds111562.mlab.com:11562/heroku_gq6tvx1v' ,
 secret: 'anysecret'
 
 }
