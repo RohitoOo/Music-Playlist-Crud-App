729e8a477b8ceafd1352c2768a3383fa81b45d7e
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-10-01 21:08:53 +0530

    Heroku Deployment

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-database:'mongodb://heroku_gq6tvx1v:d1ulefa3nhhup7akbalbaegoup@ds111562.mlab.com:11562/heroku_gq6tvx1v' ,
+database:'mongodb://rohito:Test12345@ds233541.mlab.com:33541/songsdatabase' ,
 secret: 'anysecret'
 
 }
